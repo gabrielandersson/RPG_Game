0616d0a8723a53adf44d472d60d1a42687a8b91b
Gabriel Andersson <andersson.b.gabriel@gmail.com>
2022-02-09 20:47:42 +0100

    Add project files.

---
+++ RPG_Game/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_Game
+{
+    abstract class Item
+    {
+        public string Name { get; set; }
+        public int RequiredLevel { get; set; }
+        public string Slot { get; set; }
+    }
+    class Candy : Item
+    {
+    }
+}
---
+++ RPG_Game/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RPG_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Person gg = new Person();
+            gg.Name = "lolita";
+            Console.WriteLine(gg.Name);
+        }
+    }
+    public class Person
+    {
+        public string Name { get; set; }
+    }
+}
